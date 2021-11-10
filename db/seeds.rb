e4fea05aa90603e309ff252ebe56594e4cbf46c2
Clement Azalbert <clementazalbert04@gmail.com>
2021-11-10 17:02:53 +0100

    Exercise 3 - Rails Simple Airbnb - started

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Tente Decath Super confort',
+  address: 'Porte de la Chapelle',
+  description: 'Tente ultra quali dans un quartier très animé',
+  price_per_night: 10,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Appart de ouf',
+  address: '12 rue des oufs',
+  description: 'Super appartement, très spacieux parfait pour les soirées étudiantes !',
+  price_per_night: 50,
+  number_of_guests: 15
+)
+
+Flat.create!(
+  name: 'Maison claqué au sol',
+  address: '5 rue de la maison claqué au sol',
+  description: 'Maison claqué au sol, à fuir!',
+  price_per_night: 120,
+  number_of_guests: 6
+)
+
+Flat.create!(
+  name: 'Villa Pierre Cardin',
+  address: 'dans le sud',
+  description: 'Super villa (SCH a tournée un clip là-bas)',
+  price_per_night: 1_000_000,
+  number_of_guests: 2
+)
